1c9609c44a93e3883a8c2ccff0fed8177f164b82
85desa <85desa@gmail.com>
2018-08-29 23:51:26 +0300

    r

--- converter/views.py
+++ converter/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h1>Hello world</11>")
+	return HttpResponse("<h1>Alina Pidor</11>")
 # Create your views here.
